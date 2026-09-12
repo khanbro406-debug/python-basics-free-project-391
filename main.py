d0821f0e33d0fadfcbe1b06466d3cec0c899a0fc
khanbro406-debug <khanbro406@gmail.com>
2026-09-12 13:44:32 +0000

    Добавлены рандомные значени

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from password_generator import generate_password
 
-print(generate_password(5))
-print(generate_password(30))
-print(generate_password(length=30, use_uppercase=False, use_digits=False))
-print(generate_password(length=70, use_special=True))
+print(generate_password(length=8, seed=1))
+print(generate_password(length=12, seed=123))
+print(generate_password(length=12, seed=123, use_special=True))
+print(generate_password(length=8, seed=1, use_uppercase=False, use_digits=False))
--- password_generator.py
+++ password_generator.py
@@ -2,7 +2,10 @@ LOWERCASE = "abcdefghijklmnopqrstuvwxyz"
 UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 DIGITS = "0123456789"
 SPECIAL = "!@#$%^&*"
-def generate_password(length, use_uppercase=True,use_digits=True, use_special=False):
+def next_random(number):
+    return (16807 * number) % 2147483647
+
+def generate_password(length, seed, use_uppercase=True,use_digits=True, use_special=False):
     alphabet = LOWERCASE
     if use_uppercase:
         alphabet += UPPERCASE
@@ -12,7 +15,9 @@ def generate_password(length, use_uppercase=True,use_digits=True, use_special=Fa
         alphabet += SPECIAL
     alphabet_length = len(alphabet)
     result = ""
+    current = seed
     for i in range(length):
-        index = i % alphabet_length
+        current = next_random(current)
+        index = current % alphabet_length
         result = result + alphabet[index]
     return result
