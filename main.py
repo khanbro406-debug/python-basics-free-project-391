3fb422d1e3dea3d5fb994d9e060b9c6abaa823bd
khanbro406-debug <khanbro406@gmail.com>
2026-09-12 23:41:52 +0000

    Проверка надежности

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from password_generator import generate_password
+from password_generator import check_password
 
 print(generate_password(length=8, seed=1))
 print(generate_password(length=12, seed=123))
 print(generate_password(length=12, seed=123, use_special=True))
 print(generate_password(length=8, seed=1, use_uppercase=False, use_digits=False))
 print(generate_password(length=-3, seed=42))
+
+print(check_password("abc"))
+print(check_password("abcdefgh"))
+print(check_password("abcdef1234"))
+print(check_password("Abcdef1234"))
+print(check_password("Abcdef123!"))
--- password_generator.py
+++ password_generator.py
@@ -21,3 +21,45 @@ def generate_password(length, seed, use_uppercase=True,use_digits=True, use_spec
         index = current % alphabet_length
         result = result + alphabet[index]
     return result
+def check_password(password):
+    score = 0
+    if len(password) >= 8:
+        score += 1
+    has_lower = False
+    for char in password:
+        if char in LOWERCASE:
+            has_lower = True
+            break
+    if has_lower:
+        score += 1
+    has_upper = False
+    for char in password:
+        if char in UPPERCASE:
+            has_upper = True
+        break
+    if has_upper:
+        score += 1
+    has_digit = False
+    for char in password:
+        if char in DIGITS:
+            has_digit = True
+            break
+    if has_digit: 
+        score += 1
+    has_special = False
+    for char in password:
+        if char in SPECIAL:
+            has_special = True
+            break
+    if has_special:
+        score += 1
+    if score <= 2:
+            verdict = "Слабый"
+    elif score == 3:
+        verdict = "Средний"
+    elif score == 4:
+        verdict = "Надежный"
+    else:
+        verdict = "Очень надежный"
+    return f"{verdict} пароль (оценка {score} из 5)"
+    
